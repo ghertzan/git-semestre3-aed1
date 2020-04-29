66576228f1f5b76ac8dd630d32bd3f8ac01e09fa
Gabriel Hertzan <gabriel.hertzan@gmail.com>
2020-04-29 15:06:31 -0300

    Clase Terminada, sin pruebas todavía

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,6 +1,36 @@
 /**
- * CLase que va a implementar el Ordenamiento Rápido den un arrray.
+ * CLase que va a implementar el Ordenamiento Rápido de un arrrayList.
  */
+import java.util.ArrayList;
+
 public class QuickSort{
+
+    public static void ordenar(ArrayList<Integer> items){
+
+        if(items.size() > 1){
+            ArrayList<Integer> menores = new ArrayList<>();
+            ArrayList<Integer> mayores = new ArrayList<>();
+            ArrayList<Integer> iguales = new ArrayList<>();
+
+            int pivote = items.get(items.size()-1); 
+
+            for (Integer i : items) {
+                if(i.intValue() < pivote){
+                    menores.add(i);
+                }else if( i.intValue() > pivote){
+                    mayores.add(i);
+                }else{
+                    iguales.add(i);
+                }
+            }
+            ordenar(menores);
+            ordenar(mayores);
+
+            items.clear();
+            items.addAll(menores);
+            items.addAll(iguales);
+            items.addAll(mayores);
+        }
+    }
 
 }
